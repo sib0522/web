634f22163babff38069ba25c6fdd98cc1c485e40
dasxophi <75560513+dasxophi@users.noreply.github.com>
2023-09-09 16:28:59 +0900

    update:router,main,public

--- go.mod
+++ go.mod
@@ -3,34 +3,31 @@ module GoEcho
 go 1.20
 
 require (
+	github.com/golang-jwt/jwt/v5 v5.0.0
+	github.com/google/uuid v1.3.0
+	github.com/gorilla/websocket v1.5.0
 	github.com/labstack/echo/v4 v4.10.2
 	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
-	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/gorilla/context v1.1.1 // indirect
 	github.com/gorilla/securecookie v1.1.1 // indirect
-	github.com/gorilla/websocket v1.5.0 // indirect
-	github.com/jmespath/go-jmespath v0.4.0 // indirect
 )
 
 require (
-	github.com/aws/aws-sdk-go v1.44.300
-	github.com/go-sql-driver/mysql v1.7.1 // indirect
+	github.com/go-sql-driver/mysql v1.7.1
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
-	github.com/google/uuid v1.3.0 // indirect
-	github.com/gorilla/sessions v1.2.1
+	github.com/gorilla/sessions v1.2.1 // indirect
 	github.com/labstack/echo-contrib v0.15.0
-	github.com/labstack/gommon v0.4.0 // indirect
+	github.com/labstack/gommon v0.4.0
 	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.17 // indirect
-	github.com/playree/goingtpl v0.0.0-20200611122712-24019f4df5a1 // indirect
+	github.com/mattn/go-isatty v0.0.18 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
-	golang.org/x/crypto v0.6.0 // indirect
-	golang.org/x/net v0.7.0 // indirect
-	golang.org/x/sys v0.5.0 // indirect
-	golang.org/x/text v0.7.0 // indirect
+	golang.org/x/crypto v0.9.0
+	golang.org/x/net v0.10.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
 	golang.org/x/time v0.3.0 // indirect
 )
--- router/router.go
+++ router/router.go
@@ -5,7 +5,6 @@ import (
 	"GoEcho/forms"
 	"GoEcho/logic"
 	"fmt"
-
 	"github.com/labstack/echo/v4"
 )
 
@@ -22,24 +21,44 @@ func InitRouting(e *echo.Echo) {
 
 // InitPages 各ページのルーター処理を設定する
 func InitPages(e *echo.Echo) {
+	/*
+		jwtConfig := echojwt.Config{
+			NewClaimsFunc: func(c echo.Context) jwt.Claims {
+				return new(logic.TokenData)
+			},
+			SigningKey: []byte("jwtUserLogin"),
+		}
+	*/
+
 	e.GET("/", controllers.Index)
-	e.GET("/register", controllers.Register)
-	e.GET("/login", controllers.Login)
-	e.GET("/password", controllers.Password)
-	e.GET("/file_upload", controllers.FileUpload)
-	e.GET("/delete", controllers.Delete)
-	e.GET("/tablelist", controllers.TableList)
-	e.GET("/tablelist/:name", controllers.TableDetail)
-	e.GET("/gamedbtablelist", controllers.TableList)
-	e.GET("/gamedbtablelist/:name", controllers.TableDetail)
+	e.GET("/account/register", controllers.Register)
+	e.GET("/account/login", controllers.Login)
+	e.GET("/account/password", controllers.Password)
+	e.GET("/account/delete", controllers.Delete)
+	e.GET("/file/upload", controllers.FileUpload)
+	e.GET("/file/gallery", controllers.Gallery)
+	e.GET("/table/admin", controllers.TableList)
+	e.GET("/table/admin/:name", controllers.TableDetail)
+	//e.GET("/table/game", controllers.TableList)
+	//e.GET("/table/game/:name", controllers.TableDetail)
 	e.GET("/ws", controllers.Chat)
+	e.GET("/layout/color", controllers.Layout)
+
+	/*
+		tableGroup := e.Group("/table")
+		tableGroup.Use(echojwt.WithConfig(jwtConfig))
+		//tableGroup.GET("", logic.JWTCheck)
+		tableGroup.GET("/admin", controllers.TableList)
+		tableGroup.GET("/game", controllers.TableList)
+	*/
 }
 
 // InitLogics Logic周りのルーター処理を設定する
 func InitLogics(e *echo.Echo) {
 	e.POST("/logic/signup", logic.SignUp)
-	e.POST("/logic/login", logic.Login)
+	e.POST("/account/login", logic.Login)
 	e.POST("/logic/sendmail", logic.SendMail)
 	e.POST("/logic/delete", logic.Delete)
 	e.GET("/logic/logout", logic.Logout)
+	e.POST("/upload/images", logic.UploadImage)
 }
