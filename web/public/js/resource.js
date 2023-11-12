4cb720d9f4064fb1837db4e2a76918923294599c
dasxophi <75560513+dasxophi@users.noreply.github.com>
2023-11-12 23:14:05 +0900

    update:ファイルアップロードjs

--- web/public/js/resource.js
+++ web/public/js/resource.js
@@ -13,3 +13,32 @@ function resourceDownload() {
         alert("更新失敗")
     })
 }
+
+function resourceUpload() {
+    const input = $("#fileInput")[0]
+    const files = input.files
+
+    if (files.length == 0) {
+        alert("ファイルをしてください")
+        return
+    }
+
+    let formData = new FormData()
+    for (let i = 0; i < files.length; i++) {
+        formData.append('files[]', files[i])
+    }
+
+    $.ajax({
+        type:'post',
+        url:'/resource/upload',
+        data:formData,
+        processData: false,
+        contentType: false,
+        async:false,
+    }).done(function (data){
+        alert("リソースをアップロードしました")
+        //location.replace("/")
+    }).fail(function (data){
+        alert("アップロード失敗")
+    })
+}
